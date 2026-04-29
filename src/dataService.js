6a8ad0190896ad5c9298439faad86eedd17679f5
Gumball653 <sltackle53@yahoo.com>
2026-04-29 18:07:03 -0400

    Add per-user data service with Firestore and Storage

---
+++ src/dataService.js
@@ -0,0 +1,34 @@
+import { db, storage, auth } from "./firebase";
+import { collection, doc, setDoc, getDocs, deleteDoc } from "firebase/firestore";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+
+export async function saveAnimal(animal) {
+  const user = auth.currentUser;
+  if (!user) throw new Error("Not logged in");
+
+  const refDoc = doc(db, "users", user.uid, "animals", animal.id);
+  await setDoc(refDoc, animal);
+}
+
+export async function loadAnimals() {
+  const user = auth.currentUser;
+  if (!user) return [];
+
+  const snapshot = await getDocs(collection(db, "users", user.uid, "animals"));
+  return snapshot.docs.map(d => d.data());
+}
+
+export async function deleteAnimal(id) {
+  const user = auth.currentUser;
+  const refDoc = doc(db, "users", user.uid, "animals", id);
+  await deleteDoc(refDoc);
+}
+
+export async function uploadPhoto(file, animalId) {
+  const user = auth.currentUser;
+  if (!user) throw new Error("Not logged in");
+
+  const fileRef = ref(storage, `users/${user.uid}/animals/${animalId}/${file.name}`);
+  await uploadBytes(fileRef, file);
+  return await getDownloadURL(fileRef);
+}
