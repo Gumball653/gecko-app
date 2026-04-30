f313f52c73ef59b7201ac391544d4ac34e1dcbd7
Gumball653 <sltackle53@yahoo.com>
2026-04-29 21:59:05 -0400

    Integrate QR scanner into app globally

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import LoginGate from "./LoginGate";
+import ScannerLauncher from "./ScannerLauncher";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <LoginGate>
       <App />
+      <ScannerLauncher />
     </LoginGate>
   </React.StrictMode>
 );
