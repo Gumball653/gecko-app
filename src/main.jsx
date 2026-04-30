39a0793185b58aebbfb813b56db1d4d6c3389764
Gumball653 <sltackle53@yahoo.com>
2026-04-29 22:01:57 -0400

    Enable automatic QR indexing for perfect scanning

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import LoginGate from "./LoginGate";
 import ScannerLauncher from "./ScannerLauncher";
+import QrTargetIndexer from "./QrTargetIndexer";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <LoginGate>
+      <QrTargetIndexer />
       <App />
       <ScannerLauncher />
     </LoginGate>
