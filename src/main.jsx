834defd860591bcdb1d64a4999483435de3faf66
Gumball653 <sltackle53@yahoo.com>
2026-04-29 22:56:01 -0400

    Replace broken photo system with safe uploader

--- src/main.jsx
+++ src/main.jsx
@@ -5,17 +5,19 @@ import LoginGate from "./LoginGate";
 import ScannerLauncher from "./ScannerLauncher";
 import QrTargetIndexer from "./QrTargetIndexer";
 import QrPrintPanel from "./QrPrintPanel";
-import PhotoInputOptimizer from "./PhotoInputOptimizer";
+import MobilePhotoUploader from "./MobilePhotoUploader";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <LoginGate>
-      <PhotoInputOptimizer />
       <QrTargetIndexer />
       <App />
       <ScannerLauncher />
       <QrPrintPanel />
+      <div className="fixed bottom-4 left-1/2 z-50 -translate-x-1/2 w-[min(92vw,420px)]">
+        <MobilePhotoUploader />
+      </div>
     </LoginGate>
   </React.StrictMode>
 );
