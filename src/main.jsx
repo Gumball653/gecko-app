43a47a679433a5304c5489140026ef9aaaed9bc9
Gumball653 <sltackle53@yahoo.com>
2026-04-29 22:36:49 -0400

    Add mobile photo freeze fix

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import LoginGate from "./LoginGate";
 import ScannerLauncher from "./ScannerLauncher";
 import QrTargetIndexer from "./QrTargetIndexer";
 import QrPrintPanel from "./QrPrintPanel";
+import PhotoInputOptimizer from "./PhotoInputOptimizer";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <LoginGate>
+      <PhotoInputOptimizer />
       <QrTargetIndexer />
       <App />
       <ScannerLauncher />
